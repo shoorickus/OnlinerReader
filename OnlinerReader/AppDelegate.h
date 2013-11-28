1cea74340ebdcc5ef15e2badb6399486007e0b77
Dmitry Savitskiy <dmtsavitsky@gmail.com>
2013-11-28 23:00:46 +0300

    Initial Commit

---
+++ OnlinerReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OnlinerReader
+//
+//  Created by Dmitry Savitskiy on 28/11/13.
+//  Copyright (c) 2013 Dmitry Savitskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OnlinerReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OnlinerReader
+//
+//  Created by Dmitry Savitskiy on 28/11/13.
+//  Copyright (c) 2013 Dmitry Savitskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
