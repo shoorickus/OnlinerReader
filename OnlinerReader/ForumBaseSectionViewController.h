3e1cd20c28c844988628a1db810f4c71f8cf0fef
Dmitry Savitskiy <dmitry.savitskiy@icloud.com>
2013-11-29 15:54:17 +0300

    - add libxml2 - add hpple lib for XPath - add next view controller for forum section details

---
+++ OnlinerReader/ForumBaseSectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForumBaseSectionViewController.h
+//  OnlinerReader
+//
+//  Created by Dmitry Savitskiy on 11/29/13.
+//  Copyright (c) 2013 Dmitry Savitskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForumBaseSectionViewController : UITableViewController
+
+@end
