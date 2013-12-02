75f960c8fd4beb7bac7a446871de47f69516a1e8
Dmitry Savitskiy <dmitry.savitskiy@icloud.com>
2013-12-02 23:12:05 +0300

    add forum icon
    add news icon
    add new preferences icon
    add profile icon
    add preferences view controller
    add folder with external image resources

---
+++ OnlinerReader/PreferencesViewController.h
@@ -0,0 +1,16 @@
+//
+//  PreferencesViewController.h
+//  OnlinerReader
+//
+//  Created by Dmitry Savitskiy on 12/2/13.
+//  Copyright (c) 2013 Dmitry Savitskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PreferencesViewController : UITableViewController
+
+- (IBAction)cancel:(id)sender;
+
+
+@end
---
+++ OnlinerReader/PreferencesViewController.m
@@ -0,0 +1,41 @@
+//
+//  PreferencesViewController.m
+//  OnlinerReader
+//
+//  Created by Dmitry Savitskiy on 12/2/13.
+//  Copyright (c) 2013 Dmitry Savitskiy. All rights reserved.
+//
+
+#import "PreferencesViewController.h"
+
+@interface PreferencesViewController ()
+
+@end
+
+@implementation PreferencesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)cancel:(id)sender {
+    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+}
+@end
