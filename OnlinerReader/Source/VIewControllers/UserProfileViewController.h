01170bf24b8b9820f7a3a051bb1f1d3a0eb8c538
Dmitry Savitskiy <dmitry.savitskiy@icloud.com>
2013-12-05 15:27:30 +0300

    add user profile class
    cancel button on user profile screen implementation

---
+++ OnlinerReader/Source/VIewControllers/UserProfileViewController.h
@@ -0,0 +1,15 @@
+//
+//  UserProfileViewController.h
+//  OnlinerReader
+//
+//  Created by Dmitry Savitskiy on 12/5/13.
+//  Copyright (c) 2013 Dmitry Savitskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserProfileViewController : UITableViewController
+
+- (IBAction)onCancelButtonPressed:(id)sender;
+
+@end
